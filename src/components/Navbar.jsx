fe3a3589c34c71e43f105a8b30d45acf0a20e773
tigthor <tigthor1337@gmail.com>
2021-03-09 16:23:52 +0200

    Pushing some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,33 +19,43 @@ const Navbar = () => {
         <Card className="nav_How_it_works">
           <p className="text_how_it_works">How it works</p>
           <div className="div_how_it_works">
-            <Row>
+            <Row className="row_how_it_works">
               <Col>
-                <Card>
-                  <h6></h6>
+                <Card className='how_it_works_card'>
+                  <h6>Download the app</h6>
                   <img src={how_it_works1} />
-                  <p></p>
+                  <p className="how_it_work_p">
+                    Available on Appstore and Google play
+                  </p>
                 </Card>
               </Col>
               <Col>
-                <Card>
-                  <h6></h6>
-                  <img src={how_it_works1} />
-                  <p></p>
+                <Card className="how_it_works_card">
+                  <h6>Place yor order</h6>
+                  <img src={how_it_works2} />
+                  <p className="how_it_work_p">
+                    Enter your address find what you are looking for
+                  </p>
                 </Card>
               </Col>
               <Col>
-                <Card>
-                  <h6></h6>
-                  <img src={how_it_works1} />
-                  <p></p>
+                <Card className="how_it_works_card">
+                  <h6>We handle the rest</h6>
+                  <img src={how_it_works3} />
+                  <p className="how_it_work_p">
+                    We work with the merchant to make sure everything is ready
+                    for delivery and pickup
+                  </p>
                 </Card>
               </Col>
               <Col>
-                <Card>
-                  <h6></h6>
-                  <img src={how_it_works1} />
-                  <p></p>
+                <Card className="how_it_works_card">
+                  <h6>The runner arrives</h6>
+                  <img src={how_it_works4} />
+                  <p className="how_it_work_p">
+                    ..Or you pick it up.Either way,you will get the 20 delivery
+                    points for every $1 spent
+                  </p>
                 </Card>
               </Col>
             </Row>
