8a8f8442d67ed215ebcd2bed9ecfc6688bf6a1a3
tigthor <tigthor1337@gmail.com>
2021-03-09 15:29:33 +0200

    Pushing some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,61 @@
 import React from 'react'
-import logo from '../assets/pictures/Beer.png'
 import { motion } from 'framer-motion'
-import { Card, Row } from 'antd'
+import { Card, Row, Col } from 'antd'
+import how_it_works1 from '../assets/how_it_works1.png';
+import how_it_works2 from '../assets/how_it_works2.png';
+import how_it_works3 from '../assets/how_it_works3.png';
+import how_it_works4 from '../assets/how_it_works4.png';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <i
         style={{
           width: '174px',
           height: '69px',
         }}
       />
-      <div className='links'>
-        <Card className='nav_How_it_works'>
-          <p className='text_how_it_works'>How it works</p>
+      <div className="links">
+        <Card className="nav_How_it_works">
+          <p className="text_how_it_works">How it works</p>
           <div className="div_how_it_works">
             <Row>
-              <Card>
-                <h6></h6>
-                <img src='' />
-                <p></p>
-              </Card>
-              <Card>
-                <h6></h6>
-                <img src='' />
-                <p></p>
-              </Card>
-              <Card>
-                <h6></h6>
-                <img src='' />
-                <p></p>
-              </Card>
-              <Card>
-                <h6></h6>
-                <img src='' />
-                <p></p>
-              </Card>
+              <Col>
+                <Card>
+                  <h6></h6>
+                  <img src={how_it_works1} />
+                  <p></p>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <h6></h6>
+                  <img src={how_it_works1} />
+                  <p></p>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <h6></h6>
+                  <img src={how_it_works1} />
+                  <p></p>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <h6></h6>
+                  <img src={how_it_works1} />
+                  <p></p>
+                </Card>
+              </Col>
             </Row>
           </div>
         </Card>
-        <a href='/create'>
+        <a href="/create">
           <bold> Sign in </bold>
         </a>
-        <div class='text-box'>
-          <a href='#' class='btn btn-white btn-animate'>
+        <div class="text-box">
+          <a href="#" class="btn btn-white btn-animate">
             <span
               style={{
                 color: '#EAA24D',
@@ -59,7 +70,7 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
