402f1d96c901a5ceac17815008ad9535bbaed8d7
tigthor <tigthor1337@gmail.com>
2021-03-09 14:57:20 +0200

    Pushing some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,50 @@
-import React from 'react';
-import logo from '../assets/pictures/Beer.png';
-import { motion } from 'framer-motion';
-import { Card } from 'antd';
+import React from 'react'
+import logo from '../assets/pictures/Beer.png'
+import { motion } from 'framer-motion'
+import { Card, Row } from 'antd'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className='navbar'>
       <i
         style={{
           width: '174px',
           height: '69px',
-          // paddingLeft: '92px',
-          // paddingBottom: '29px',
         }}
       />
-      <div className="links">
-        <Card className="nav_How_it_works">
-          <p>How it works</p>
-          <div>
+      <div className='links'>
+        <Card className='nav_How_it_works'>
+          <p className='text_how_it_works'>How it works</p>
+          <div className="div_how_it_works">
             <Row>
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
+              <Card>
+                <h6></h6>
+                <img src='' />
+                <p></p>
+              </Card>
+              <Card>
+                <h6></h6>
+                <img src='' />
+                <p></p>
+              </Card>
+              <Card>
+                <h6></h6>
+                <img src='' />
+                <p></p>
+              </Card>
+              <Card>
+                <h6></h6>
+                <img src='' />
+                <p></p>
+              </Card>
             </Row>
           </div>
         </Card>
-        <a href="/create">
+        <a href='/create'>
           <bold> Sign in </bold>
         </a>
-        <div class="text-box">
-          <a href="#" class="btn btn-white btn-animate">
+        <div class='text-box'>
+          <a href='#' class='btn btn-white btn-animate'>
             <span
               style={{
                 color: '#EAA24D',
@@ -45,7 +59,7 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
