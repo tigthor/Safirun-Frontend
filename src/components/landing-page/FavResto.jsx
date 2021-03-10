c08fb842c4ef1ad0c09418402380780839feabf5
tigthor <tigthor1337@gmail.com>
2021-03-10 14:53:52 +0200

    Pushing some changes

--- src/components/landing-page/FavResto.jsx
+++ src/components/landing-page/FavResto.jsx
@@ -1,25 +1,28 @@
+import { Space } from 'antd';
 import React from 'react';
 const Favresto = () => {
     return (
       <div className="favoriteRestoPar">
-        <div style={{ marginRight: '200px', marginTop: '90px' }}>
-          <h3
-            class="fontF"
-            style={{
-              textAlign: 'left',
-              fontStyle: 'normal',
-              fontWeight: '600px',
-              fontSize: '35px',
-              lineHeight: '35px',
-            }}
-          >
-            Favorite Restaurants &<br></br> Stores in your pocket
-          </h3>
-          <p style={{ textAlign: 'left' }}>
-            Order from your favorite restaurants and stores,<br></br>with the
-            all-new SAFIRUN app.
-          </p>
-          <h4 style={{ textAlign: 'left' }}>DISCOVER. ORDER. TRACK IN APP</h4>
+        <div style={{ marginRight: '200px', marginTop: '150px' }}>
+          <Space direction="vertical" size="middle">
+            <h3
+              class="fontF"
+              style={{
+                textAlign: 'left',
+                fontStyle: 'normal',
+                fontWeight: '600px',
+                fontSize: '35px',
+                lineHeight: '35px',
+              }}
+            >
+              Favorite Restaurants &<br></br> Stores in your pocket
+            </h3>
+            <p style={{ textAlign: 'left' }}>
+              Order from your favorite restaurants and stores,<br></br>with the
+              all-new SAFIRUN app.
+            </p>
+            <h4 style={{ textAlign: 'left' }}>DISCOVER. ORDER. TRACK IN APP</h4>
+          </Space>
           <img src="../assets/PlaystoreBadges-011.jpg"></img>
           <img src="../assets/PlaystoreBadges-021.jpg"></img>
         </div>
--- src/components/landing-page/marketplace.jsx
+++ src/components/landing-page/marketplace.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { MdArrowForward } from 'react-icons/md';
 
 const Marketplace = () => {
-
   const Data = [
     {
       url: '../../assets/food1.png',
@@ -35,102 +34,91 @@ const Marketplace = () => {
       backgroudColor: '#004CE7',
     },
   ];
-    return (
-      <div>
-        <div className="marketPlaceChil1">
-          <h1 className="light">SAFIRUN market place </h1>
-          <div style={{ display: 'flex', position: 'relative' }}>
-            <p
-              className="viewMore"
-              style={{ marginRight: '20px', marginBottom: '-40px' }}
-            >
-              Vew more
-            </p>
-            <p>
-              <MdArrowForward
-                style={{
-                  color: 'black',
-                  height: '27px',
-                  width: '27px',
-                  marginLeft: '80px',
-                  marginTop: '-12px',
-                  position: 'absolute',
-                  left: '10px',
-                  top: '18px',
-                  backgroundColor: '#CCCCCC',
-                  borderRadius: '20px',
-                }}
-              />
-            </p>
-          </div>
+  return (
+    <div>
+      <div className="marketPlaceChil1">
+        <h1 className="light">SAFIRUN market place </h1>
+        <div style={{ display: 'flex', position: 'relative', flexDirection: 'row'}}>
+          <p
+            className="viewMore"
+            style={{ marginRight: '20px', marginBottom: '-40px' }}
+          >
+            View more
+          </p>
+          <p>
+            <MdArrowForward
+            className='ViewMore_arrow'
+               type='button'
+            />
+          </p>
         </div>
-        <div className="marketPlaceChil2">
-          {Data.map((data) => (
-            <div className="marketPlaceChil3">
-              <img
-                src={data.url}
-                style={{
-                  width: '342px',
-                  height: '303px',
-                  left: '539px',
-                    borderRadius: '3px',
-                  top: '1002px',
-                }}
-              />
-              <p
-                style={{
-                  backgroundColor: `${data.backgroudColor}`,
-                  width: '342px',
-
-                  height: '57px',
-                  fontFamily: 'poppis',
-                  fontStyle: 'normal',
-                  fontWeight: 'bold',
-                  fontSize: '15px',
-                  lineHeight: '33px',
-                  position: 'absolute',
-                  bottom: '-4px',
-                  left: '0px',
-                  borderRadius: '3px',
-                  color: '#FFFFFF',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                {data.text}
-              </p>
-            </div>
-          ))}
+      </div>
+      <div className="marketPlaceChil2">
+        {Data.map((data) => (
           <div className="marketPlaceChil3">
             <img
-              src="../assets/SHOPPIN2.png"
+              src={data.url}
               style={{
-                width: '1042px',
-                height: '246px',
-                  borderRadius: '3px',
+                width: '342px',
+                height: '303px',
                 left: '539px',
+                borderRadius: '3px',
                 top: '1002px',
               }}
             />
             <p
               style={{
-                backgroundColor: '#750062',
-                width: '1042px',
+                backgroundColor: `${data.backgroudColor}`,
+                width: '342px',
                 color: '#FFFFFF',
                 position: 'absolute',
                 justifyContent: 'center',
+                textAlign: 'center',
+                verticalAlign: 'middle',
                 bottom: '-4px',
                 borderRadius: '3px',
                 left: '-0px',
                 height: '57px',
+                lineHeight: '57px',
               }}
             >
-              SHOPPIG
+              {data.text}
             </p>
           </div>
+        ))}
+        <div className="marketPlaceChil3">
+          <img
+            src="../assets/SHOPPIN2.png"
+            style={{
+              width: '1042px',
+              height: '246px',
+              borderRadius: '3px',
+              left: '539px',
+              top: '1002px',
+            }}
+          />
+          <p
+            style={{
+              backgroundColor: '#750062',
+              width: '1042px',
+              color: '#FFFFFF',
+              position: 'absolute',
+              justifyContent: 'center',
+              textAlign: 'center',
+              verticalAlign: 'middle',
+              bottom: '-4px',
+              borderRadius: '3px',
+              left: '-0px',
+              height: '57px',
+              lineHeight: '57px',
+            }}
+          >
+            SHOPPING
+          </p>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Marketplace;
