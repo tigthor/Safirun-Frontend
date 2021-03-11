ee18c288f56d244767835b70006d75e3209149b8
tigthor <tigthor1337@gmail.com>
2021-03-11 08:50:46 +0200

    Pushing some changes

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,17 +1,21 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-webpack',
+  ],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
-    // "bundle": true,
+    "bundle": true,
   },
   packageOptions: {
     /* ... */
--- src/components/landing-page/Readyorder.jsx
+++ src/components/landing-page/Readyorder.jsx
@@ -5,89 +5,95 @@ import location from '../../assets/pictures/location.png';
 import { Button, Divider, Card, Input, Space, Row } from 'antd';
 
 const Readyorder = () => {
-    return (
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'right',
+        flexWrap: 'wrap',
+        maxWidth: '1440px',
+        height: '500px',
+        backgroundColor: '#ffff',
+      }}
+    >
       <div
-        style={{
-          display: 'flex',
-          justifyContent: 'right',
-          flexWrap: 'wrap',
-          maxWidth: '1440px',
-          height: '500px',
-          backgroundColor: '#ffff',
-        }}
+        className="readyToOrder"
+        style={{ width: '718px', marginTop: '100px' }}
       >
-        <div
-          className="readyToOrder"
-          style={{ width: '718px', marginTop: '100px' }}
-        >
-          <h2>Ready To Order</h2>
-          <p>
-            Browser local restaurants and stores for<br></br>delively by enterig
-            your adress below
-          </p>
-          <div>
-            <img
-              src="../assets/pin.png"
-              style={{
-                width: '27px',
-                height: '27px',
-                position: 'absolute',
-                marginTop: '15px',
-              }}
-            ></img>
-
-            <div className="SearchBox">
-              <Input
-                className="SearchInput"
-                style={{
-                  width: '497.58px',
-                  top: '10px',
-                  right: '3300px',
-                  height: '44.59px',
-                  margin: 'center',
-                  borderRadius: '10px',
-                  borderColor: 'transparent',
-                  textAlign: 'center',
-                }}
-                placeholder="Enter Delivery Address to See The Menu"
-              ></Input>
-              <Button
-                type="primary"
-                className="SearchArrow"
-                icon={
-                  <img
-                    style={{
-                      height: '50px',
-                      position: 'absolute',
-                      width: '58.42px',
-                      left: '-2.5px',
-                      bottom: '-3.5px',
-                    }}
-                    src={Arrow}
-                  />
-                }
-              ></Button>
-              <img
-                src={location}
-                style={{
-                  position: 'absolute',
-                  left: '6px',
-                  top: '9.5px',
-                  height: '30px',
-                  width: '38.42px',
-                }}
-              />
-            </div>
-          </div>
-        </div>
+        <h2>Ready To Order</h2>
+        <p>
+          Browser local restaurants and stores for<br></br>delively by enterig
+          your adress below
+        </p>
         <div>
           <img
-            src="../assets/order.png"
-            style={{ position: 'absolute', width: '500px', height: '400px' }}
-          />
+            src="../assets/pin.png"
+            style={{
+              width: '27px',
+              height: '27px',
+              position: 'absolute',
+              marginTop: '15px',
+            }}
+          ></img>
+
+          <div
+            style={{
+              display: 'flex',
+              position: 'relative',
+              height: '47.59px',
+            }}
+          >
+            <Input
+              className="SearchInput"
+              style={{
+                width: '497.58px',
+                top: '10px',
+                right: '3300px',
+                height: '44.59px',
+                margin: 'center',
+                borderRadius: '10px',
+                borderColor: 'transparent',
+                textAlign: 'center',
+              }}
+              placeholder="Enter Delivery Address to See The Menu"
+            ></Input>
+            <Button
+              type="primary"
+              className="SearchArrow"
+              icon={
+                <img
+                  style={{
+                    height: '50px',
+                    position: 'absolute',
+                    width: '58.42px',
+                    left: '-2.5px',
+                    bottom: '-3.5px',
+                  }}
+                  src={Arrow}
+                />
+              }
+            ></Button>
+            <img
+              src={location}
+              style={{
+                position: 'absolute',
+                left: '6px',
+                top: '9.5px',
+                height: '30px',
+                width: '38.42px',
+              }}
+            />
+          </div>
         </div>
       </div>
-    );
-}
+      <div>
+        <img
+          src="../assets/order.png"
+          style={{ position: 'absolute', width: '500px', height: '400px' }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Readyorder;
