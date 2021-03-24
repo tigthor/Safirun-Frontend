d79fdaed619261353dcd7b573e1998742406f7b8
tigthor <tightor1337@gmail.com>
2021-03-24 16:06:00 +0200

    Safirun Front-end

--- src/components/landing-page/Readyorder.jsx
+++ src/components/landing-page/Readyorder.jsx
@@ -11,7 +11,9 @@ const Readyorder = () => {
                 flexWrap: 'wrap',
                 maxWidth: '1340px',
                 height: '400px',
-                backgroundColor: '#ffff'
+                backgroundColor: '#ffff',
+                marginLeft: 'auto',
+                marginRight: 'auto'
             }}
         >
             <div
